738719819d056dbfa63a3d16dea90c0c5f5b21fa
rafeeq-123 <rafeeq-123@hotmail.com>
2016-07-17 22:46:45 -0700

    I am making a lot of changes to the cli interface. i also decided to make list of the books 1-7 for both ebooks and audiobooks

--- lib/pottermore/cli.rb
+++ lib/pottermore/cli.rb
@@ -10,14 +10,13 @@ class Pottermore::CLI
     ebooks
   end
 
-   def self.ebooks
-   doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/ebooks/us-english"))
-   @ebooks = doc.css("div.product-summary__title").text
-   @ebc = doc.css("div.product-summary__title")[0].text
-   @@list << self.ebooks
-   @@list
+  def self.ebooks
+
+  # @@list << self.ebooks
+  # @@list
    binding.pry
   end
+
 
   def start_menu
     puts " What would you like to do? Press 1 to look at Ebooks, press 2 to look at Audiobook, press 3 to look at the lastest news and press 4 to see upcoming events..."
@@ -26,18 +25,37 @@ class Pottermore::CLI
       input = gets.strip.downcase
       case input
       when "1"
-        puts "Here are the available Ebooks:"
-        @ebooks
         puts "Would you like to look at the collection of all 7 instead Y/N?"
       when "y"
         puts "Here is the collection:"
-        @ebc
+        puts "#{book_collection}:"
       when "n"
         puts "Here are the available Ebooks:"
-        @ebooks
+        puts "*************************************************************"
+        puts "Book one:" "#{book_one}"
+        puts "Book two:" "#{book_two}"
+        puts "Book three:" "#{book_three}"
+        puts "Book four:" "#{book_four}"
+        puts "Book five:" "#{book_five}"
+        puts "Book six:" "#{book_six}"
+        puts "Book seven:" "#{book_seven}"
+        @ebooks #maybe I want to list the books 1-7?
         #want to add an option that allow the client to also choose the whole collection of HP instead of just one book
       when "2"
+        puts "Would you like to look at the collection of all 7 instead Y/N?"
+      when "y"
+        puts "Here is the collection:"
+        puts "#{audiobook_collection}:"
+      when "n"
         puts "Here are the available Audiobooks:"
+        puts "*************************************************************"
+        puts "Book one:" "#{audiobook_one}"
+        puts "Book two:" "#{audiobook_two}"
+        puts "Book three:" "#{audiobook_three}"
+        puts "Book four:" "#{audiobook_four}"
+        puts "Book five:" "#{audiobook_five}"
+        puts "Book six:" "#{audiobook_six}"
+        puts "Book seven:" "#{audiobook_seven}"
       when "3"
         puts "Here is the lastest Potter related news!"
       when "4"
--- lib/pottermore/scraper.rb
+++ lib/pottermore/scraper.rb
@@ -2,30 +2,49 @@ require 'open-uri'
 require 'pry' #having a had time making binding.pry work.
 require 'nokogiri'
 
-class Pottermore::Scraper
+class Scraper
 
-  attr_accessor :url,
+  attr_accessor :url
 
   def initalize
     @ebooks = ebooks
     @audiobooks = audiobooks
     @news = news
-    @ebc = ebc #ebook collection
-    @abc = abc #audiobook collection
-    @@list = []
+ #   @ebc = ebc #ebook collection
+  #  @abc = abc #audiobook collection
+   # @@list = []
   end
 
   def self.ebooks
    doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/ebooks/us-english"))
-   @ebooks = doc.css("div.product-summary__title").text
-   @ebc = doc.css("div.product-summary__title")[0].text
-   @@list << self.ebooks
-   @@list
+   book_one = doc.css("div.product-summary__title")[1].text
+   book_two = doc.css("div.product-summary__title")[2].text
+   book_three = doc.css("div.product-summary__title")[3].text
+   book_four = doc.css("div.product-summary__title")[4].text
+   book_five = doc.css("div.product-summary__title")[5].text
+   book_six = doc.css("div.product-summary__title")[6].text
+   book_seven = doc.css("div.product-summary__title")[7].text
+
+   #
+   book_collection = doc.css("div.product-summary__title")[0].text
+  # @@list << self.ebooks
+  # @@list
    binding.pry
   end
 
   def self.audiobooks
+   doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/audio-books/us-english"))
+   audiobook_one = doc.css("div.product-summary__title")[1].text
+   audiobook_two = doc.css("div.product-summary__title")[2].text
+   audiobook_three = doc.css("div.product-summary__title")[3].text
+   audiobook_four = doc.css("div.product-summary__title")[4].text
+   audiobook_five = doc.css("div.product-summary__title")[5].text
+   audiobook_six = doc.css("div.product-summary__title")[6].text
+   audiobook_seven = doc.css("div.product-summary__title")[7].text
 
+   #
+   audiobook_collection = doc.css("div.product-summary__title")[0].text
+   binding.pry
   end
 
   def self.news
