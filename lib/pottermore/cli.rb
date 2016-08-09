c989383d5dc8e217371db8eef8edb5dda874b57e
rafeeq-123 <rafeeq-123@hotmail.com>
2016-08-09 13:59:30 -0700

    DRYing out my code with wrapping up nokogiri in a method

--- lib/pottermore/cli.rb
+++ lib/pottermore/cli.rb
@@ -7,56 +7,73 @@ class Pottermore::CLI
   def call
     @all_books = Pottermore::Scraper.new
     puts "Welcome to Pottermore!"
+    puts "Would you like to see  ebooks 1, audiobooks 2, enhanced books 3, gifts 4 or misalencous 5 ? Type would you would like!!"
     start_menu
   end
 
   def start_menu
-
-    input = nil
+    input = ""
     while input != "exit"
-      input = gets.strip.downcase
+
+    input = gets.chomp.downcase
       case input
-
-
-
+      when "1"
+        ebook_display
+        message
+      when "2"
+        audiobook_display
+        message
+      when "3"
+        enhanced_display
+        message
+      when "4"
+        list_of_souvenirs
+        message
+      when "5"
+        list_awards
+        message
+      end
+    end
   end
 
   def ebook_display
      @all_books.ebook_title.each_with_index do |book_title, index|
-      puts "Ebook #{index+1} #{book_title} price: #{@all_books.ebook_price[index+2]}"
+      puts "Ebook #{index+1} #{book_title} price: #{@all_books.ebook_price[index+1]}"
     end
   end
 
   def audiobook_display
     @all_books.audiobook_title.each_with_index do |book_title, index|
-      puts "Ebook #{index+1} #{book_title} price: #{@all_books.audiobook_price[index+2]}"
+      puts "Audiobooks #{index+1} #{book_title} price: #{@all_books.audiobook_price[index+2]}"
     end
   end
 
   def enhanced_display
       @all_books.hd_title.each_with_index do |book_title, index|
-      puts "Ebook #{index+1} #{book_title} price: #{@all_books.hd_price[index+2]}"
+      puts "Enhanced #{index+1} #{book_title} price: #{@all_books.hd_price[index+0]}"
     end
   end
 
   def list_of_souvenirs
-    @all_books.all_souvenirs.each_with_index do |souvenir, index|
-      puts "List of souvenirs #{index+1} #{souvenir}" "price: #{@all_books.all_souvenir_price[index+0]}"
+    @all_books.souvenirs.each_with_index do |souvenir, index|
+      puts "List of souvenirs #{index+1} #{souvenir} price: #{@all_books.souvenir_price[index+0]}"
      end
    end
 
   def list_awards
-    @all_books.all_awards.each_with_index do |award, index|
+    @all_books.awards.each_with_index do |award, index|
       puts "Award #{index+1} #{award}"
      end
    end
 
-   def numbers
-    numbers = %w[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-   end
+  def message
+    puts "*****************************************************************"
+    puts "Would you like to see  ebooks 1, audiobooks 2, enhanced books 3, gifts 4 or misalencous 5 ? Type would you would like!!"
+    puts "*****************************************************************"
   end
+
  end
-end
+
 
 
 
--- lib/pottermore/scraper.rb
+++ lib/pottermore/scraper.rb
@@ -4,35 +4,49 @@ require 'nokogiri'
 
 class Pottermore::Scraper
 
-  def initalize
-     @all_books = @all_books
+attr_accessor :title, :price, :award, :souvenir
+
+  def initalize(args)
+     @all_books = args[:all_books]
+     @price = args[:price]
+     @title = args[:title]
+     @award = args[:award]
+     @souvenir = args[:souvenir]
+  end
+
+  def audio_noko
+    doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/audio-books/us-english"))
   end
 
   def audiobook_title
-    doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/audio-books/us-english"))
-      doc.collect do |ebook|
-        title.text
-      end
-    end
-
-  # def audiobook_price
-  #   doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/audio-books/us-english")).css("span.product-summary__price")
-  #     doc.collect do |price|
-  #       price.text
-  #     end
-  #   end
-
-  def ebook_title
-    doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/ebooks/us-english")).css("div.product-summary__title")
+    audio_noko.css("div.product-summary__title")
       doc.collect do |title|
         title.text
       end
     end
 
-  def ebook_price
-    doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/ebooks/us-english")).css("span.product-summary__price")
+  def audiobook_price
+    audio_noko.css("span.product-summary__price")
       doc.collect do |price|
         price.text
+    end
+  end
+
+  def ebook_noko
+    doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/ebooks/us-english"))
+  end
+
+  def ebook_title
+     ebook_noko.css("div.product-summary__title")
+      doc.collect do |title|
+        title.text
+    end
+  end
+
+  def ebook_price
+    ebook_noko.css("span.product-summary__price")
+      doc.collect do |price|
+       price.text
       end
     end
 
@@ -46,7 +60,7 @@ class Pottermore::Scraper
   def hd_price
     doc = Nokogiri::HTML(open("https://usd.shop.pottermore.com/collections/enhanced-editions")).css("span.product-summary__price")
       doc.collect do |price|
-        price.text
+         price.text
       end
     end
 
@@ -56,19 +70,19 @@ class Pottermore::Scraper
     end
   end
 
-  def all_awards
+  def awards
     Nokogiri::HTML(open("http://www.harrypotter.bloomsbury.com/uk/jkrowling/awards/")).css("div.award-list.row li").collect do |award|
      award.text
     end
   end
 
-  def all_souvenirs
+  def souvenirs
     Nokogiri::HTML(open("https://cursedchildstore.com/collections/souvenirs")).css("h3.product-title").collect do |souvenir|
       souvenir.text
     end
   end
 
-  def all_souvenir_price
+  def souvenir_price
     Nokogiri::HTML(open("https://cursedchildstore.com/collections/souvenirs")).css("p.product-price").collect do |price|
       price.text
     end
